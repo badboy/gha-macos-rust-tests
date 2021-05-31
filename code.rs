684ff916f805f17e58d298e0ee03e309a58a2e90
Jan-Erik Rediger <janerik@fnordig.de>
2021-05-31 13:33:19 +0200

    init

---
+++ code.rs
@@ -0,0 +1,4 @@
+#[no_mangle]
+pub extern "C" fn hello_world() -> u32 {
+    1+1
+}
---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -xe
+
+commit=ab06beff0b87a039398867d10c9e6570a1d5efc4
+
+cargo install rustup-toolchain-install-master
+
+rustup-toolchain-install-master $commit
+
+rustc +$commit \
+  --crate-type=staticlib
+  --target aarch64-apple-ios-sim \
+  code.rs 
+
+ls -lh
+tool -l *.a
